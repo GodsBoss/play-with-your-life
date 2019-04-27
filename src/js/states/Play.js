81f3c9d7347ae38532442bd476fd3e35b3df7fbb
Timo Reitz <godsboss@gmx.de>
2019-04-27 05:06:00 +0200

    Introduce Score class.

--- src/js/states/Play.js
+++ src/js/states/Play.js
@@ -23,3 +23,22 @@ export default class Play extends State {
 
   invoke(game, event) {}
 }
+
+class Score {
+  constructor() {
+    this.wealth = 0
+    this.accomplishment = 0
+    this.wellBeing = 0
+  }
+
+  add(otherScore) {
+    this.wealth += +otherScore.wealth
+    this.accomplishment += +otherScore.accomplishment
+    this.wellBeing += +otherScore.wellBeing
+  }
+
+  // Calculates the resulting total score.
+  total() {
+    return this.wealth * this.accomplishment * this.wellBeing
+  }
+}
