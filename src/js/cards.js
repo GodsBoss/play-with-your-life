935f8984cf6392ecb0f9f43f47a53bcb23ecfe47
Timo Reitz <godsboss@gmx.de>
2019-04-27 09:19:50 +0200

    Add package which contains the cards.

---
+++ src/js/cards.js
@@ -0,0 +1,24 @@
+const cards = [
+  {
+    title: "party hard!",
+    cost: {
+      time: 1,
+      health: 5
+    },
+    benefits: {
+      pleasure: 10
+    }
+  },
+  {
+    title: "do drugs!",
+    cost: {
+      time: 1,
+      health: 8
+    },
+    benefits: {
+      pleasure: 15
+    }
+  }
+]
+
+export default cards
