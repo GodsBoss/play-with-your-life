b9ae33d936e9621034bb2a5142119695f285ee97
Timo Reitz <godsboss@gmx.de>
2019-04-27 03:04:43 +0200

    Generate everything via Webpack.

---
+++ webpack.config.js
@@ -0,0 +1,50 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  mode: 'production',
+  entry: './src/js/index.js',
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    host: '0.0.0.0'
+  },
+  plugins: [
+    new CleanWebpackPlugin(['dist']),
+    new HtmlWebpackPlugin(
+      {
+        title: 'Ludum Dare 44 (working title)'
+      }
+    ),
+    new CopyWebpackPlugin(
+      [
+        {
+          from: "src/gfx/*.png",
+          to: "[name].[ext]"
+        }
+      ]
+    )
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: [/node_modules/],
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+            plugins: [
+              require('@babel/plugin-proposal-export-default-from')
+            ]
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist')
+  }
+};
