ea5e94bd2100ac5be4fbf6c7aebce4067bf5d2bd
Timo Reitz <godsboss@gmx.de>
2019-04-27 06:05:39 +0200

    Generate font sprite infos.

--- src/js/sprites.js
+++ src/js/sprites.js
@@ -1,18 +1,69 @@
-const sprites = [
-  {
-    type: "bg",
-    x: 1280,
-    y: 0,
-    w: 320,
-    h: 200
-  },
-  {
-    type: "screen_title",
-    x: 1280,
-    y: 200,
-    w: 320,
-    h: 200
+const sprites = [].concat(
+  [
+    {
+      type: "bg",
+      x: 1280,
+      y: 0,
+      w: 320,
+      h: 200
+    },
+    {
+      type: "screen_title",
+      x: 1280,
+      y: 200,
+      w: 320,
+      h: 200
+    }
+  ],
+  createFontSprites()
+)
+
+function createFontSprites() {
+  // Generate sprite info for letters A to Z.
+  const charCodeA = 'A'.charCodeAt(0)
+  const charCodeZ = 'Z'.charCodeAt(0)
+  const chars = []
+  for(var offset = 0; offset < charCodeZ - charCodeA; offset++) {
+    chars.push(
+      {
+        type: 'char_' + String.fromCharCode(charCodeA + offset),
+        x: offset * 5,
+        y: 0,
+        w: 5,
+        h: 5
+      }
+    )
   }
-]
+
+  // Generate sprite info for numbers 0 to 9.
+  for(var i = 0; i < 10; i++) {
+    chars.push(
+      {
+        type: 'char_' + i,
+        x: (26 + i - 1) * 5,
+        y: 0,
+        w: 5,
+        h: 5
+      }
+    )
+  }
+
+  // Generate sprite info for some punctuation.
+  '.,:!?-="()'.split('').forEach(
+    (char, index) => {
+      chars.push(
+        {
+          type: 'char_' + char,
+          x: (26 + 10 + index - 1) * 5,
+          y: 0,
+          w: 5,
+          h: 5
+        }
+      )
+    }
+  )
+
+  return chars
+}
 
 export default sprites
