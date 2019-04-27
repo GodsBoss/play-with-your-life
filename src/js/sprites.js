94592d5e5efd929a9298438f4f44884cdd6f79b0
Timo Reitz <godsboss@gmx.de>
2019-04-27 04:59:58 +0200

    Add sprite infos for title and play screen.

--- src/js/sprites.js
+++ src/js/sprites.js
@@ -1,3 +1,18 @@
-const sprites = []
+const sprites = [
+  {
+    type: "bg",
+    x: 1280,
+    y: 0,
+    w: 320,
+    h: 200
+  },
+  {
+    type: "screen_title",
+    x: 1280,
+    y: 200,
+    w: 320,
+    h: 200
+  }
+]
 
 export default sprites
