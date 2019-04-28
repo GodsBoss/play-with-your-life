1064d70c84cd4cf608e2430f7206b0a95b7d1aa5
Timo Reitz <godsboss@gmx.de>
2019-04-28 10:51:55 +0200

    Show highscores.

--- src/js/states/Highscores.js
+++ src/js/states/Highscores.js
@@ -1,3 +1,4 @@
+import { line, LEFT, RIGHT } from '../font'
 import State from '../State'
 
 export default class Highscores extends State {
@@ -29,6 +30,48 @@ export default class Highscores extends State {
         z: 0
       }
     )
+    game.objects.push(
+      {
+        type: 'score_wealth',
+        x: 50,
+        y: TOP,
+        w: 9,
+        h: 9,
+        z: 1000
+      }
+    )
+    game.objects.push(
+      {
+        type: 'score_accomplishment',
+        x: 110,
+        y: TOP,
+        w: 9,
+        h: 9,
+        z: 1000
+      }
+    )
+    game.objects.push(
+      {
+        type: 'score_pleasure',
+        x: 170,
+        y: TOP,
+        w: 9,
+        h: 9,
+        z: 1000
+      }
+    )
+    game.objects.push(
+      ...line(
+        {
+          message: 'TOTAL',
+          position: { x: 230, y: TOP + 2 }
+        }
+      )
+    )
+    showList(game, { x:  65, y: TOP + 15}, game.data.highscores.lists.wealth.values)
+    showList(game, { x: 125, y: TOP + 15}, game.data.highscores.lists.accomplishment.values)
+    showList(game, { x: 185, y: TOP + 15}, game.data.highscores.lists.pleasure.values)
+    showList(game, { x: 259, y: TOP + 15}, game.data.highscores.lists.total.values)
   }
 
   invoke(game, event) {
@@ -37,3 +80,25 @@ export default class Highscores extends State {
     }
   }
 }
+
+function showList(game, position, list) {
+  console.log(game, position, list)
+  list.forEach(
+    (value, index) => {
+      game.objects.push(
+        ...line(
+          {
+            message: value.toString(),
+            position: {
+              x: position.x,
+              y: position.y + index * 7
+            },
+            align: RIGHT
+          }
+        )
+      )
+    }
+  )
+}
+
+const TOP = 50
