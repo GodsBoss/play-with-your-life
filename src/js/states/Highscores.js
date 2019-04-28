553a54f1c9a761d7a3462b0088c3d87642251569
Timo Reitz <godsboss@gmx.de>
2019-04-28 10:00:54 +0200

    Add (yet empty) screen for highscores.

---
+++ src/js/states/Highscores.js
@@ -0,0 +1,39 @@
+import State from '../State'
+
+export default class Highscores extends State {
+  constructor({ nextState, background, size }) {
+    super()
+    this.nextState = nextState
+    this.background = background
+    this.size = size
+  }
+
+  init(game) {
+    game.objects.push(
+      {
+        type: this.background,
+        x: 0,
+        y: 0,
+        w: this.size.width,
+        h: this.size.height,
+        z: -10000
+      }
+    )
+    game.objects.push(
+      {
+        type: 'screen_highscores',
+        x: 0,
+        y: 0,
+        w: this.size.width,
+        h: this.size.height,
+        z: 0
+      }
+    )
+  }
+
+  invoke(game, event) {
+    if (event.type === 'click') {
+      game.nextState(this.nextState)
+    }
+  }
+}
