4f9583c89ecb68e46a7cb8f046781682fbe7b8d3
Timo Reitz <godsboss@gmx.de>
2019-04-28 09:12:37 +0200

    Add stuff to handle highscores.

---
+++ src/js/Highscores.js
@@ -0,0 +1,33 @@
+export default class Highscores {
+  constructor({ maxListLength }) {
+    this.lists = {
+      wealth: new List({ maxLength: maxListLength }),
+      accomplishment: new List({ maxLength: maxListLength }),
+      pleasure: new List({ maxLength: maxListLength }),
+      total: new List({ maxLength: maxListLength })
+    }
+  }
+
+  add(score) {
+    this.lists.wealth.add(score.wealth)
+    this.lists.accomplishment.add(score.accomplishment)
+    this.lists.pleasure.add(score.pleasure)
+    this.lists.total.add(score.total())
+  }
+}
+
+class List {
+  constructor({ maxLength }) {
+    this.maxLength = maxLength
+    this.values = []
+  }
+
+  add(value) {
+    this.values.push(value)
+    this.values.sort()
+    this.values = this.values.reverse()
+    if (this.values.length > this.maxLength) {
+      this.values = this.values.slice(0, -1)
+    }
+  }
+}
